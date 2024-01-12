ba463512a27205dfc5c824cb2f24edd11c9bdecf
Theo-radj <156411698+Theo-radj@users.noreply.github.com>
2024-01-12 22:52:16 +0100

    Update interface.py

--- game/interface.py
+++ game/interface.py
@@ -1,9 +1,28 @@
 #IMPORT
 import pygame
+pygame.init()
 
 
 #CLASS
+class interface() :
+    def __init__(self, screen_size=(1080,720) ) :
+      # générer la fenêtre de notre jeux 
+      self.nom = pygame.display.set_caption("Anger Snake")
+      self.taille = pygame.display.set_mode(screen_size)
 
+    def update_interface(self) :
+      running = True
+
+      # boucle permettant de rester la fenetre allumé
+      while running :
+
+        for event in pygame.event.get() :
+          
+          # verifier si le joueur ferme la fenêtre
+          if event.type == pygame.QUIT:
+            running = False
+            pygame.quit
+        
 
 
 
@@ -15,23 +34,10 @@ import pygame
 
 
 #MAIN
+if __name__ == "__main__":
+  Screen = interface()
+  Screen.update_interface()
 
-pygame.init()
-
-#générer la fenêtre de notre jeux 
-pygame.display.set_caption("Anger Snake")
-pygame.display.set_mode((960,540))
-
-running = True
-
-while running :
-  #screen.blit(background,(0.0))
-  pygame.disflip.flip()
-
-  for event in pygame.event.get() :
-    if event.type == pygame.QUIT:
-      running = False
-      pygame.quit
 
 
 
