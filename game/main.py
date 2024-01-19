e69979fead3ebe7d0a7519c28a4444adbf9cd4fc
Theo-radj <156411698+Theo-radj@users.noreply.github.com>
2024-01-19 22:01:11 +0100

    Update main.py

--- game/main.py
+++ game/main.py
@@ -7,14 +7,16 @@ import random
 
 
 
-nb_x = 720
-nb_y = 720
 
-grille =  [[0 for y in range(nb_x)] for x in range(nb_y)]
+nb_x = 45
+nb_y = 45
+
+
 pos_joueur = (random.randint(0,nb_x),random.randint(0,nb_y))
-grille[pos_joueur[0]-1][pos_joueur[1]-10] = 1
+
 
 direction = "DROITE"
+
 #CLASS
 class interface() :
   def __init__(self, screen_size=(720,720)) :
@@ -26,44 +28,76 @@ class interface() :
     self.ecran = pygame.display.set_mode(self.size)
     self.surface_dessin = pygame.Surface((180, 150))
 
+
   def update_interface_ouvert(self) :
     # boucle permettant de rester la fenetre allumé
       for event in pygame.event.get() :
         # verifier si le joueur ferme la fenêtre
         if event.type == pygame.QUIT:
-          return False
+
           pygame.quit()
+          return False
           sys.exit
       return True
-          
+
+  def analyse_grille(self):
+    global grille
+    for i in range(len(grille)):
+      for j in range(len(grille[i])):
+        if grille[i][j] == 2 :
+          self.dessine_rocher(i,j)
+        if grille[i][j] == 1 :
+          self.dessine_perso(i,j)
+
+
   def dessine_serpent(self, serpent, couleur):
     for i in serpent :
-      pygame.draw.rect(self.ecran, couleur, (200, 200,10,10))
-      pygame.display.flip()
+      pygame.draw.rect(self.ecran, couleur, (i[0], i[1],64,64))
+    pygame.display.flip()
+
+  def dessine_rocher(self, x,y):
+    pygame.draw.rect(self.ecran, ((255,255,255)), (x*16, y*16,16,16))
+    pygame.display.flip()
+  def dessine_perso(self,x,y):
+    pygame.draw.rect(self.ecran, ((0,255,0)), (x*16, y*16,16,16))
+    pygame.display.flip()
 
 
 class perso() :
-  def __init__(self, joueur, interface):
+  def __init__(self, joueur):
     self.joueur = joueur
-    self.perso = (10,10)
     self.couleur = (0, 255, 0)
-    self.interface = interface
-
+    grille[self.joueur[0]][self.joueur[1]] = 1
+  def déplacement(self,direction):
+    global grille
+    grille[self.joueur[0]][self.joueur[1]] = 0
+    if direction =="HAUT":
+      self.joueur[1] += 1
+    elif direction =="BAS":
+      self.joueur[1] -= 1
+    elif direction =="GAUCHE":
+      self.joueur[0] -= 1
+    elif direction =="DROITE":
+      self.joueur[0] += 1
+    grille[self.joueur[0]][self.joueur[1]] = 1
 
 class ennemi() :
-  def __init__(self, couleur, interface):
+  def __init__(self, couleur, interface, grille):
     self.couleur = couleur
     self.interface = interface
     self.taille = random.randint(2,5)
+    self.grille = grille
 
     k,j = random.randint(3,72), random.randint(0,72)
     self.serpent = [(k*10 , j*10), (k*10-10, j*10), (k*10-20, j*10)]
-     
 
-  def Serpent(self, direction) :
+
+  def Serpent(self) :
+    global direction , grille
     for i in self.serpent :
-      grille[i[0]][i[1]] = 1
       print(i)
+      self.grille[i[0]][i[1]] = 1
+    print(grille)
 
     tete = list(self.serpent[0])
 
@@ -75,7 +109,7 @@ class ennemi() :
       tete[0] -= 10
     elif direction == "DROITE":
       tete[0] += 10
-    
+
 
     if tete[0] < 0 or tete[1] < 0 :
       pygame.quit()
@@ -86,42 +120,52 @@ class ennemi() :
 
     self.interface.ecran.fill((0,0,0))
 
-    for i in self.serpent :
-      pygame.draw.rect(self.interface.ecran, self.couleur, (i[0], i[1],10,10))
-    pygame.display.flip()
-    
-    return direction
+    self.interface.dessine_serpent(self.serpent, self.couleur)
+
 
 
 #FONCTION
-def control(direction):
+def control():
   keys = pygame.key.get_pressed()
   if keys:
-    if keys[pygame.K_UP] and not direction == "BAS":
-      direction = "HAUT"
-      direction = mechant1.Serpent(direction)
+    if keys[pygame.K_UP]:
+      perso.déplacement("HAUT")
+
 
     elif keys[pygame.K_DOWN] and not direction == "HAUT":
-      direction = "BAS"
-      direction = mechant1.Serpent(direction)
+      perso.déplacement("BAS")
+
 
     elif keys[pygame.K_LEFT] and not direction == "DROITE":
-      direction = "GAUCHE"
-      direction = mechant1.Serpent(direction)
+      perso.déplacement("GAUCHE")
+
 
     elif keys[pygame.K_RIGHT] and not direction == "GAUCHE":
-      direction = "DROITE"
-      direction = mechant1.Serpent(direction)
-    
-  return direction
+      perso.déplacement("DROITE")
+
+
+def gener_map():
+    map = [[0 for _ in range(100)] for _ in range(100)]
+    for x in range (3):
+        for i in range(100):
+            for j in range(100):
+                if map[(i+1)%100][(j+1)%100] == 2 or map[(i+1)%100][j] == 2 or map[i][(j+1)%100] == 2 or map[(i-1)%100][j] ==2 or map[(i-1)%100][(j-1)%100] or map[i][(j-1)%100] ==2:
+                    chance = 600
+                    print("chanceux")
+                else:
+                    chance = 998
+                    print("malchanceux")
+                if random.randint(0,1000) > chance:
+                    map[i][j] = 2
+    return map
 
 
 #MAIN
 if __name__ == "__main__":
-  
+  grille = gener_map()
   inter = interface()
-  mechant1 = ennemi((0,255,0),inter)
-  personnage = perso(pos_joueur, inter)
+  #mechant1 = ennemi((0,255,0),inter)
+  personnage = perso(pos_joueur)
 
   clock = pygame.time.Clock()
 
@@ -129,7 +173,7 @@ if __name__ == "__main__":
 
   while run :
     run = inter.update_interface_ouvert()
-    direction = control(direction)
-
+    control()
+    inter.analyse_grille()
     clock.tick(30)
 
