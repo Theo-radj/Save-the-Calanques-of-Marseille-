05cbed07ca17510041f5f985aedc3727ef2da226
Theo-radj <156411698+Theo-radj@users.noreply.github.com>
2024-02-07 09:37:27 +0100

    Add files via upload

---
+++ game/asset/stjomad-game-main (1)/stjomad-game-main/README.md
@@ -0,0 +1,3 @@
+# stjomad-game
+# Introduction: 
+This is a simple text based adventure game. The purpose of this game is for you, the player, to navigate through different environments and interact with various objects
---
+++ game/asset/stjomad-game-main (1)/stjomad-game-main/game/hero.py
@@ -0,0 +1,76 @@
+﻿import random
+import pygame
+from pathfinding import*
+
+class perso() :
+  def __init__(self, nb,map):
+    self.joueur = [random.randint(0,nb -1 ),random.randint(0,nb - 1)]
+    self.couleur = (0, 255, 0)
+    map[self.joueur[1]][self.joueur[0]] = 1
+    self.direction = " "
+    self.pierre_time = 0
+    self.old_ticks_time = 0
+    self.est_mort = False
+
+  def déplacement(self, direction, grille):
+    self.direction = direction
+    grille[self.joueur[1]][self.joueur[0]] = 0
+    if self.direction =="HAUT":
+      self.joueur[1] -= 1
+    elif self.direction =="BAS":
+      self.joueur[1] += 1
+    elif self.direction =="GAUCHE":
+      self.joueur[0] -= 1
+    elif self.direction =="DROITE":
+      self.joueur[0] += 1
+    grille[self.joueur[1]][self.joueur[0]] = 1
+
+
+  def casser_pierre(self, grille, inter) :
+    time = pygame.time.get_ticks() - self.old_ticks_time
+    self.old_ticks_time = pygame.time.get_ticks()
+    self.pierre_time += time
+
+    if self.direction =="HAUT" and self.joueur[1]-1 <= len(grille)-1 and grille[self.joueur[1]-1][self.joueur[0]] != 0:
+        if self.pierre_time > 200 :
+            if grille[self.joueur[1]-1][self.joueur[0]] == 4:
+                inter.score += 1
+            grille[self.joueur[1]-1][self.joueur[0]] -= 3
+            self.pierre_time = 0
+
+            if grille[self.joueur[1]-1][self.joueur[0]] < 10:
+                grille[self.joueur[1]-1][self.joueur[0]]  = 0
+
+    elif self.direction =="BAS" and self.joueur[1]+1 <= len(grille)-1 and grille[self.joueur[1]+1][self.joueur[0]] != 0:
+        if self.pierre_time > 200 :
+            if grille[self.joueur[1]+1][self.joueur[0]] == 4:
+                inter.score += 1
+            grille[self.joueur[1]+1][self.joueur[0]] -= 3
+            self.pierre_time = 0
+
+        if grille[self.joueur[1]+1][self.joueur[0]] < 10:
+            grille[self.joueur[1]+1][self.joueur[0]] = 0
+
+    elif self.direction =="GAUCHE"  and self.joueur[0]-1 <= len(grille[0])-1 and grille[self.joueur[1]][self.joueur[0]-1] != 0:
+        if self.pierre_time > 200 :
+            if grille[self.joueur[1]][self.joueur[0-1]] == 4:
+                    inter.score += 1
+            grille[self.joueur[1]][self.joueur[0]-1] -= 3
+            self.pierre_time = 0
+
+            if grille[self.joueur[1]][self.joueur[0]-1] < 10:
+                grille[self.joueur[1]][self.joueur[0]-1] = 0
+
+    elif self.direction =="DROITE" and self.joueur[0]+1 <= len(grille[0])-1 and grille[self.joueur[1]][self.joueur[0]+1] != 0 :
+        if self.pierre_time > 200 :
+            if grille[self.joueur[1]][self.joueur[0+1]] == 4:
+                    inter.score += 1
+            grille[self.joueur[1]][self.joueur[0]+1] -= 3
+            self.pierre_time = 0
+        if grille[self.joueur[1]][self.joueur[0]+1] < 10:
+            grille[self.joueur[1]][self.joueur[0]+1] = 0
+
+    print(inter.score)
+
+
+
